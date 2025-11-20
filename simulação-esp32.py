6696049d91ed94b6c8fee2f06c46776502114044
nathanokz <nathan.okazaki@pucpr.edu.br>
2025-11-20 14:55:30 -0300

    Update simulação-esp32.py

--- simulação-esp32.py
+++ simulação-esp32.py
@@ -1,60 +1,97 @@
+import network
+import socket
+import time
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
-import time
+import _thread
 
 i2c = I2C(0, scl=Pin(4), sda=Pin(5))
 oled = SSD1306_I2C(128, 64, i2c)
 
-def loading():
-    for i in range(0,5):
-        oled.text('loading.',24,28)
-        oled.fill_rect(25,40,26,8,1)
-        oled.show()
-        time.sleep(0.3)
-        oled.fill(0)
-        
-        oled.text('loading..',24,28)
-        oled.fill_rect(25,40,51,8,1)
-        oled.show()
-        time.sleep(0.3)
-        oled.fill(0)
-        
-        oled.text('loading...',24,28)
-        oled.fill_rect(25,40,76,8,1)
-        oled.show()
-        time.sleep(0.3)
-        oled.fill(0)
+rede = network.WLAN(network.STA_IF)
+rede.active(True)
 
-def enviar_dados():
+rede.connect('Baco', 'maicon142020')
+
+while not rede.isconnected():
+    print('conectando ao wifi...')
+    time.sleep(1)
+    
+print('conectado!')
+
+host = '192.168.1.105'
+porta = 5000
+
+metodoSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+metodoSocket.connect((host, porta))
+
+print('conectado ao servidor tcp!')
+
+def receber_mensagens():
+    while True:
+        data = metodoSocket.recv(1024)
+        if data:
+            mensagem = data.decode().strip()
+            print('mensagem do servidor: ', mensagem)
+            
+            if mensagem == '/led_on':
+                oled.fill(1)
+                oled.show()
+            elif mensagem == '/led_off':
+                oled.fill(0)
+                oled.show()
+            time.sleep(0.01)
+        else:
+            pass
+
+_thread.start_new_thread(receber_mensagens,())
+
+for i in range(0,5):
+    oled.text('loading.',24,28)
+    oled.fill_rect(25,40,26,8,1)
+    oled.show()
+    time.sleep(0.3)
+    oled.fill(0)
+    
+    oled.text('loading..',24,28)
+    oled.fill_rect(25,40,51,8,1)
+    oled.show()
+    time.sleep(0.3)
+    oled.fill(0)
+    
+    oled.text('loading...',24,28)
+    oled.fill_rect(25,40,76,8,1)
+    oled.show()
+    time.sleep(0.3)
+    oled.fill(0)
+    
+for j in range(0,3):
     oled.text('enviando dados.',0,28)
     oled.show()
     time.sleep(0.5)
     oled.fill(0)
-    
+
     oled.text('enviando dados..',0,28)
     oled.show()
     time.sleep(0.5)
     oled.fill(0)
-    
+
     oled.text('enviando dados...',0,28)
     oled.show()
     time.sleep(0.5)
     oled.fill(0)
-        
-    for i in range(0,5):
-        oled.text('esp32_client.ino',0,0)
-        oled.text('{"type": "data",',0,10)
-        oled.text('"from": "esp32",',0,20)
-        oled.text('"payload":',0,30)
-        oled.text('{"temp": 25.3,',0,40)
-        oled.text('"hum": 60}}',0,50)
-        oled.show()
-        time.sleep(2)
-        oled.fill(0)
-        oled.show()
-        time.sleep(2)
-        
-loading()
-enviar_dados()
 
+while True:
+    mensagemesp = '{"type": "data", "from": "esp32", "payload": {"temp": "25.3", "hum": "60}}"'
+    metodoSocket.send(mensagemesp.encode())
+    oled.text('esp32_client.ino',0,0)
+    oled.text('{"type": "data",',0,10)
+    oled.text('"from": "esp32",',0,20)
+    oled.text('"payload":',0,30)
+    oled.text('{"temp": 25.3,',0,40)
+    oled.text('"hum": 60}}',0,50)
+    oled.show()
+    time.sleep(2)
 
+print('fechando conexão...')
+metodoSocket.close()
